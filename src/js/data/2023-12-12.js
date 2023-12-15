2a6ffc1cad129135e8fbe855b245acb43fbf424c
Joao Felipe Gobeti <joao.gobeti@motora.ai>
2023-12-15 16:08:44 -0300

    done

--- src/js/data/2023-12-12.js
+++ src/js/data/2023-12-12.js
@@ -119,4 +119,994 @@ dataSet[dataSetVersion].characterData = [
       artists: ["Alic"]      
     }
   },
+{
+  name: "Yours Truly - Ariana Grande",
+  img: "qHzGsrt.jpg",
+  opts: {
+    artists: ["Aria"]
+  }
+},
+{
+  name: "My Everything - Ariana Grande",
+  img: "SyvAUH4.png",
+  opts: {
+    artists: ["Aria"]
+  }
+},
+{
+  name: "Dangerous Woman - Ariana Grande",
+  img: "RJYCUyw.jpg",
+  opts: {
+    artists: ["Aria"]
+  }
+},
+{
+  name: "Sweetener - Ariana Grande",
+  img: "6O4y6Vw.jpg",
+  opts: {
+    artists: ["Aria"]
+  }
+},
+{
+  name: "Thank U, Next - Ariana Grande",
+  img: "xIwy3ux.jpg",
+  opts: {
+    artists: ["Aria"]
+  }
+},
+{
+  name: "Positions - Ariana Grande",
+  img: "ApHjEhP.png",
+  opts: {
+    artists: ["Aria"]
+  }
+},
+{
+  name: "Magic Christian Music - Badfinger",
+  img: "aovUJpf.jpg",
+  opts: {
+    artists: ["Badf"]
+  }
+},
+{
+  name: "No Dice - Badfinger",
+  img: "iPDxhpd.jpg",
+  opts: {
+    artists: ["Beat"]
+  }
+},
+{
+  name: "Please Please Me - The Beatles",
+  img: "lU4n9Y9.jpg",
+  opts: {
+    artists: ["Beat"]
+  }
+},
+{
+  name: "With The Beatles - The Beatles",
+  img: "nNXb7Rp.jpg",
+  opts: {
+    artists: ["Beat"]
+  }
+},
+{
+  name: "A Hard Days Night - The Beatles",
+  img: "g1lTU5O.jpg",
+  opts: {
+    artists: ["Beat"]
+  }
+},
+{
+  name: "Beatles For Sala - The Beatles",
+  img: "MoZZtqB.jpg",
+  opts: {
+    artists: ["Beat"]
+  }
+},
+{
+  name: "Help! - The Beatles",
+  img: "hCAoj5i.jpg",
+  opts: {
+    artists: ["Beat"]
+  }
+},
+{
+  name: "Rubber Soul - The Beatles",
+  img: "HrFCKJn.jpg",
+  opts: {
+    artists: ["Beat"]
+  }
+},
+{
+  name: "Revolver - The Beatles",
+  img: "JqapArG.jpg",
+  opts: {
+    artists: ["Beat"]
+  }
+},
+{
+  name: "Sgt Pepper Lonely Hearts Club Band - The Beatles",
+  img: "iW9qi06.jpg",
+  opts: {
+    artists: ["Beat"]
+  }
+},
+{
+  name: "Magical Mistery Tour - The Beatles",
+  img: "6vAMEC8.jpg",
+  opts: {
+    artists: ["Beat"]
+  }
+},
+{
+  name: "White Album - The Beatles",
+  img: "EGlEswH.jpg",
+  opts: {
+    artists: ["Beat"]
+  }
+},
+{
+  name: "Abbey Road - The Beatles",
+  img: "bQnWuoc.jpg",
+  opts: {
+    artists: ["Beat"]
+  }
+},
+{
+  name: "Let It Be - The Beatles",
+  img: "QMtyDQD.jpg",
+  opts: {
+    artists: ["Beat"]
+  }
+},
+{
+  name: "Alucinação - Belchior",
+  img: "vhSrkid.jpg",
+  opts: {
+    artists: ["Belc"]
+  }
+},
+{
+  name: "Lemonade - Beyoncé",
+  img: "x5SvvTN.jpg",
+  opts: {
+    artists: ["Beyo"]
+  }
+},
+{
+  name: "Happier Than Ever - Billie Eilish ",
+  img: "VWX1WXU.jpg",
+  opts: {
+    artists: ["Bill"]
+  }
+},
+{
+  name: "When We All Fall Asleep, Where Do You Go? - Billie Eilish",
+  img: "BefIpD2.jpg",
+  opts: {
+    artists: ["Bill"]
+  }
+},
+{
+  name: "The Freewheelin - Bob Dylan",
+  img: "FQfxGlf.jpg",
+  opts: {
+    artists: ["BobD"]
+  }
+},
+{
+  name: "Another Side Of Bob Dylan - Bob Dylan",
+  img: "xf9H6p2.jpg",
+  opts: {
+    artists: ["BobD"]
+  }
+},
+{
+  name: "Bringing It All Back Home - Bob Dylan",
+  img: "yltkIww.jpg",
+  opts: {
+    artists: ["BobD"]
+  }
+},
+{
+  name: "Highway 61 Revisited - Bob Dylan",
+  img: "VeheIps.jpg",
+  opts: {
+    artists: ["BobD"]
+  }
+},
+{
+  name: "New Morning - Bob Dylan",
+  img: "UFpqxBF.jpg",
+  opts: {
+    artists: ["BobD"]
+  }
+},
+{
+  name: "Blood On The Tracks - Bob Dylan",
+  img: "BgR0LWy.jpg",
+  opts: {
+    artists: ["BobD"]
+  }
+},
+{
+  name: "Time Out Of Mind - Bob Dylan",
+  img: "9OW3lKH.jpg",
+  opts: {
+    artists: ["BobD"]
+  }
+},
+{
+  name: "The Record - Boygenius",
+  img: "2dya84N.jpg",
+  opts: {
+    artists: ["Boyg"]
+  }
+},
+{
+  name: "Blackout - Britney Spears",
+  img: "krc1SDq.jpg",
+  opts: {
+    artists: ["Brit"]
+  }
+},
+{
+  name: "Ofertório - Caetano Veloso",
+  img: "7U1vQdw.jpg",
+  opts: {
+    artists: ["Caet"]
+  }
+},
+{
+  name: "Transa - Caetano Veloso",
+  img: "i7OIwDx.jpg",
+  opts: {
+    artists: ["Caet"]
+  }
+},
+{
+  name: "Abraçaço - Caetano Veloso",
+  img: "v1LhNIy.jpg",
+  opts: {
+    artists: ["Caet"]
+  }
+},
+{
+  name: "Tapestry - Carole King",
+  img: "H4X82zx.jpg",
+  opts: {
+    artists: ["CKin"]
+  }
+},
+{
+  name: "Crash - Charli XCX",
+  img: "n4wFdje.jpg",
+  opts: {
+    artists: ["CXCX"]
+  }
+},
+{
+  name: "Construção - Chico Buarque",
+  img: "uVvKtEI.jpg",
+  opts: {
+    artists: ["CBua"]
+  }
+},
+{
+  name: "The Rise and Fall Of Ziggy Stardust - David Bowie",
+  img: "P1F9Wak.jpg",
+  opts: {
+    artists: ["DBow"]
+  }
+},
+{
+  name: "Hunky Dory - David Bowie",
+  img: "pAVJEGM.jpg",
+  opts: {
+    artists: ["DBow"]
+  }
+},
+{
+  name: "Layla and assorted love song - Derek and The Dominos",
+  img: "TVpqelB.jpg",
+  opts: {
+    artists: ["DATD"]
+  }
+},
+{
+  name: "Planet Her - Doja Cat",
+  img: "jwiVNHW.jpg",
+  opts: {
+    artists: ["DCat"]
+  }
+},
+{
+  name: "Future Nostalgia - Dua Lipa",
+  img: "5xzTRsP.jpg",
+  opts: {
+    artists: ["DuaL"]
+  }
+},
+{
+  name: "O Melhor de Edson Gomes - Edson Gomes",
+  img: "ncLtLLZ.jpg",
+  opts: {
+    artists: ["EGom"]
+  }
+},
+{
+  name: "Rumors - Fleetwood Mac",
+  img: "ERbCtQD.jpg",
+  opts: {
+    artists: ["Flee"]
+  }
+},
+{
+  name: "Franz Ferdinand - Franz Ferdinand",
+  img: "DeUrEAE.jpg",
+  opts: {
+    artists: ["FFer"]
+  }
+},
+{
+  name: "All Things Must Pass - George Harrison",
+  img: "9vDmd9c.jpg",
+  opts: {
+    artists: ["GHar"]
+  }
+},
+{
+  name: "Living In The Material World - George Harrison",
+  img: "RGbwjoy.jpg",
+  opts: {
+    artists: ["GHar"]
+  }
+},
+{
+  name: "Dark Horse - George Harrison ",
+  img: "xrrbju7.jpg",
+  opts: {
+    artists: ["GHar"]
+  }
+},
+{
+  name: "Extra Texture - George Harrison",
+  img: "anPQ9I4.jpg",
+  opts: {
+    artists: ["GHar"]
+  }
+},
+{
+  name: "Thirty Three & ⅓ - George Harrison",
+  img: "ThDfZTH.jpg",
+  opts: {
+    artists: ["GHar"]
+  }
+},
+{
+  name: "George Harrison - George Harrison",
+  img: "VNPqFTC.jpg",
+  opts: {
+    artists: ["GHar"]
+  }
+},
+{
+  name: "Somewhere in England - George Harrison",
+  img: "iInkFDI.jpg",
+  opts: {
+    artists: ["GHar"]
+  }
+},
+{
+  name: "Gone Troppo - George Harrison ",
+  img: "jPRImsw.jpg",
+  opts: {
+    artists: ["GHar"]
+  }
+},
+{
+  name: "Cloud Nine - George Harrison ",
+  img: "8QTg1cG.jpg",
+  opts: {
+    artists: ["GHar"]
+  }
+},
+{
+  name: "Brainwashed - George Harrison",
+  img: "BAmgUes.jpg",
+  opts: {
+    artists: ["GHar"]
+  }
+},
+{
+  name: "Realce - Gilberto Gil",
+  img: "fiDgMuj.jpg",
+  opts: {
+    artists: ["GGil"]
+  }
+},
+{
+  name: "Appetite For Destruction - Guns N Roses",
+  img: "O4nuJM3.jpg",
+  opts: {
+    artists: ["GNRS"]
+  }
+},
+{
+  name: "Harry Styles - Harry Styles",
+  img: "DO2l43a.jpg",
+  opts: {
+    artists: ["HSty"]
+  }
+},
+{
+  name: "Fine Line - Harry Styles",
+  img: "TEmj2xJ.jpg",
+  opts: {
+    artists: ["HSty"]
+  }
+},
+{
+  name: "Harry’s House - Harry Styles",
+  img: "Ziqmza4.jpg",
+  opts: {
+    artists: ["HSty"]
+  }
+},
+{
+  name: "Plastic Ono Band - John Lennon e Yoko Ono",
+  img: "IZLy1iL.jpg",
+  opts: {
+    artists: ["Joko"]
+  }
+},
+{
+  name: "Imagine - John Lennon e Yoko Ono",
+  img: "zpH1pup.jpg",
+  opts: {
+    artists: ["Joko"]
+  }
+},
+{
+  name: "Double Fantasy - John Lennon e Yoko Ono",
+  img: "m8gv5gr.jpg",
+  opts: {
+    artists: ["Joko"]
+  }
+},
+{
+  name: "La La Land - Justin Hurwitz",
+  img: "kvRpEHn.jpg",
+  opts: {
+    artists: ["JHur"]
+  }
+},
+{
+  name: "Teenage Dream - Katy Perry",
+  img: "p4uDY4t.jpg",
+  opts: {
+    artists: ["KPer"]
+  }
+},
+{
+  name: "The Fame Monster - Lady Gaga",
+  img: "gVckhLa.jpg",
+  opts: {
+    artists: ["Gaga"]
+  }
+},
+{
+  name: "Born This Way - Lady Gaga",
+  img: "Sw9qai4.jpg",
+  opts: {
+    artists: ["Gaga"]
+  }
+},
+{
+  name: "Did You Know That there's a tunnel under ocean blvd - Lana Del Rey ",
+  img: "0oQPgEa.jpg",
+  opts: {
+    artists: ["Lana"]
+  }
+},
+{
+  name: "Blue Bannisters - Lana Del Rey",
+  img: "H5wfrDJ.jpg",
+  opts: {
+    artists: ["Lana"]
+  }
+},
+{
+  name: "Chemtrails Over The Country Club - Lana Del Rey",
+  img: "H93nQMZ.jpg",
+  opts: {
+    artists: ["Lana"]
+  }
+},
+{
+  name: "Norman Fucking Rockwell - Lana Del Rey",
+  img: "Fgobiwc.jpg",
+  opts: {
+    artists: ["Lana"]
+  }
+},
+{
+  name: "Lust For Life - Lana Del Rey",
+  img: "nW4ULue.jpg",
+  opts: {
+    artists: ["Lana"]
+  }
+},
+{
+  name: "Honeymoon - Lana Del Rey",
+  img: "9kjFSiY.jpg",
+  opts: {
+    artists: ["Lana"]
+  }
+},
+{
+  name: "Ultraviolence - Lana Del Rey",
+  img: "4Zw4hYY.jpg",
+  opts: {
+    artists: ["Lana"]
+  }
+},
+{
+  name: "Born to die - Lana Del Rey",
+  img: "05UPw1I.jpg",
+  opts: {
+    artists: ["Lana"]
+  }
+},
+{
+  name: "The Miseducation Of Lauryn Hill  - Lauryn Hill",
+  img: "wOSsP5B.jpg",
+  opts: {
+    artists: ["LHil"]
+  }
+},
+{
+  name: "Special - Lizzo",
+  img: "j3rebST.jpg",
+  opts: {
+    artists: ["Lizz"]
+  }
+},
+{
+  name: "Solar Power - Lorde",
+  img: "n4ZmZt0.jpg",
+  opts: {
+    artists: ["Lord"]
+  }
+},
+{
+  name: "Melodrama - Lorde",
+  img: "dytHKbM.jpg",
+  opts: {
+    artists: ["Lord"]
+  }
+},
+{
+  name: "Pure Heroine - Lorde",
+  img: "QnTeqZQ.jpg",
+  opts: {
+    artists: ["Lord"]
+  }
+},
+{
+  name: "Thriller - Michael Jackson",
+  img: "XLmrFKF.jpg",
+  opts: {
+    artists: ["MJac"]
+  }
+},
+{
+  name: "Endless Summer Vacation - Miley Cyrus",
+  img: "ENkLFm2.jpg",
+  opts: {
+    artists: ["MCyr"]
+  }
+},
+{
+  name: "Plastic Hearts - Miley Cyrus",
+  img: "9Ph7wX5.jpg",
+  opts: {
+    artists: ["MCyr"]
+  }
+},
+{
+  name: "The Land Is Inhospitable and so are we - Mistki",
+  img: "2ArTA4G.jpg",
+  opts: {
+    artists: ["Mits"]
+  }
+},
+
+{
+  name: "MTV Unplugged - Nirvana",
+  img: "JfhnYUv.jpg",
+  opts: {
+    artists: ["Nirv"]
+  }
+},
+{
+  name: "Acabou Chorare - Novos Baianos",
+  img: "zNB7CdZ.jpg",
+  opts: {
+    artists: ["NBai"]
+  }
+},
+{
+  name: "Definitely Maybe - Oasis",
+  img: "WzfKmAU.jpg",
+  opts: {
+    artists: ["Oasi"]
+  }
+},
+{
+  name: "(What’s The Story) Morning Glory- Oasis",
+  img: "fDXwC01.jpg",
+  opts: {
+    artists: ["Oasi"]
+  }
+},
+{
+  name: "SOUR - Olivia Rodrigo",
+  img: "TrXzmVZ.jpg",
+  opts: {
+    artists: ["ORod"]
+  }
+},
+{
+  name: "Guts - Olivia Rodrigo",
+  img: "kpZEKcG.jpg",
+  opts: {
+    artists: ["ORod"]
+  }
+},
+{
+  name: "FOUR - One Direction",
+  img: "zA8mNvW.jpg",
+  opts: {
+    artists: ["ODir"]
+  }
+},
+{
+  name: "Midnight Memories - One Direction",
+  img: "DmqqSVi.jpg",
+  opts: {
+    artists: ["ODir"]
+  }
+},
+{
+  name: "Made In The AM - One Direction",
+  img: "lKfnBmk.jpg",
+  opts: {
+    artists: ["ODir"]
+  }
+},
+{
+  name: "McCartney - Paul McCartney",
+  img: "JkV6vqM.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "RAM - Paul McCartney ",
+  img: "MOEvpOU.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "Band On The Run - Paul McCartney",
+  img: "RfESgjR.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "Wings Over America - Paul McCartney",
+  img: "gKorw5T.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "McCartney II - Paul McCartney",
+  img: "BvjqcZb.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "Tug Of War - Paul McCartney",
+  img: "l7BSCI7.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "Pipes Of Peace - Paul McCartney",
+  img: "KBLG7gE.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "Give My Regards to Broad Street - Paul McCartney",
+  img: "u5F6zDe.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "Flowers In The Dirt - Paul McCartney",
+  img: "0k6sGR2.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "Chaos And Creation In The Backyard - Paul McCartney",
+  img: "zGAixFU.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "McCartney III - Paul McCartney",
+  img: "SOdygPI.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "New - Paul McCartney",
+  img: "uGVOAGZ.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "Egypt Station - Paul McCartney",
+  img: "g9VQeWd.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "Flaming Pie - Paul McCartney",
+  img: "QhuAXov.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "Venus And Mars - Paul McCartney",
+  img: "wovbQzk.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "Stranger In The Alps - Phoebe Brigders",
+  img: "JVJxUGb.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "Punisher - Phoebe Brigders",
+  img: "J5AflrY.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "The Dark Side Of The Moon - Pink Floyd",
+  img: "nvnOFKG.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "Animals - Pink Floyd",
+  img: "WeFU5bH.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "The Wall - Pink Floyd",
+  img: "o0Vj4Ry.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "Wish You Were Here - Pink Floyd",
+  img: "DWSFsPQ.jpg",
+  opts: {
+    artists: ["PMcC"]
+  }
+},
+{
+  name: "Admirável Chip Novo - Pitty",
+  img: "OgOGtx9.jpg",
+  opts: {
+    artists: ["Pitt"]
+  }
+},
+{
+  name: "A Night At The Opera - Queen",
+  img: "ef4agXF.jpg",
+  opts: {
+    artists: ["Quee"]
+  }
+},
+{
+  name: "Ok Computer - Radiohead",
+  img: "Hw6IH8F.jpg",
+  opts: {
+    artists: ["Radi"]
+  }
+},
+{
+  name: "The Bends - Radiohead",
+  img: "T7Q2vHM.jpg",
+  opts: {
+    artists: ["Radi"]
+  }
+},
+{
+  name: "Let it Bleed - The Rolling Stones",
+  img: "RQSwafP.jpg",
+  opts: {
+    artists: ["TRSt"]
+  }
+},
+{
+  name: "Motomami - Rosalia",
+  img: "PUgXirP.jpg",
+  opts: {
+    artists: ["Rosa"]
+  }
+},
+{
+  name: "Wonder - Shawn Mendes",
+  img: "nca4tPL.jpg",
+  opts: {
+    artists: ["SMen"]
+  }
+},
+{
+  name: "Shawn Mendes - Shawn Mendes",
+  img: "qMvMZk0.jpg",
+  opts: {
+    artists: ["SMen"]
+  }
+},
+{
+  name: "Illuminate - Shawn Mendes",
+  img: "ejWfEdH.jpg",
+  opts: {
+    artists: ["SMen"]
+  }
+},
+{
+  name: "Songs In The Key Of Life - Stevie Wonder",
+  img: "M7LnSQk.jpg",
+  opts: {
+    artists: ["SWon"]
+  }
+},
+{
+  name: "The New Abnormal - The Strokes",
+  img: "dLD3uof.jpg",
+  opts: {
+    artists: ["TStr"]
+  }
+},
+{
+  name: "Carrie & Lowell - Sufjan Stevens",
+  img: "JcC7rNW.jpg",
+  opts: {
+    artists: ["SSte"]
+  }
+},
+{
+  name: "Javelin - Sufjan Stevens",
+  img: "S75GfS4.jpg",
+  opts: {
+    artists: ["SSte"]
+  }
+},
+{
+  name: "Stop Making Sense - Talking Heads",
+  img: "UmmYO1G.jpg",
+  opts: {
+    artists: ["THea"]
+  }
+},
+{
+  name: "1989 (Taylor's Versions) - Taylor Swift",
+  img: "IjHVBjD.jpg",
+  opts: {
+    artists: ["TSwi"]
+  }
+},
+{
+  name: "Speak Now (Taylor's Versions) - Taylor Swift",
+  img: "15nVlmw.jpg",
+  opts: {
+    artists: ["TSwi"]
+  }
+},
+{
+  name: "Red - (Taylor's Versions) - Taylor Swift",
+  img: "Fe6v9vt.jpg",
+  opts: {
+    artists: ["TSwi"]
+  }
+},
+{
+  name: "Fearless (Taylor's Versions) - Taylor Swift",
+  img: "Vf80vNb.jpg",
+  opts: {
+    artists: ["TSwi"]
+  }
+},
+{
+  name: "Reputation - Taylor Swift",
+  img: "HLj3yrL.jpg",
+  opts: {
+    artists: ["TSwi"]
+  }
+},
+{
+  name: "Folklore - Taylor Swift",
+  img: "hCF8UVG.jpg",
+  opts: {
+    artists: ["TSwi"]
+  }
+},
+{
+  name: "Evermore - Taylor Swift",
+  img: "UD118wF.jpg",
+  opts: {
+    artists: ["TSwi"]
+  }
+},
+{
+  name: "Midnights - Taylor Swift",
+  img: "uubJOpv.jpg",
+  opts: {
+    artists: ["TSwi"]
+  }
+},
+{
+  name: "Lover - Taylor Swift",
+  img: "oUbB952.jpg",
+  opts: {
+    artists: ["TSwi"]
+  }
+},
+{
+  name: "Tribalistas 1 - Tribalistas",
+  img: "nuE995x.jpg",
+  opts: {
+    artists: ["Trib"]
+  }
+},
+{
+  name: "Tribalistas 2 - Tribalistas",
+  img: "sRIhNjY.jpg",
+  opts: {
+    artists: ["Trib"]
+  }
+},
+
+
 ];
